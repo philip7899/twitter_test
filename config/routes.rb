5f6be4d90905f8324ffb185ab81c2133d18308aa
Philip Stitzer <pghrpg@gmail.com>
2014-10-16 14:17:05 -0400

    changed initial routing settings and created static_pages controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ TwitterTest::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'static_pages#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
