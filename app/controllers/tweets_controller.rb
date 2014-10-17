b42f6242ee60ac166e67d9ef5a813cd2f0b8fee5
Philip Stitzer <pghrpg@gmail.com>
2014-10-16 20:04:25 -0400

    tweet create controller action works

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,15 @@ class TweetsController < ApplicationController
 	def new
 		@tweet = Tweet.new
 	end
+
+	def create
+		@tweet = Tweet.create(tweet_params)
+		render 'new'
+	end
+
+	private
+
+	def tweet_params
+		params.require(:tweet).permit(:content)
+	end
 end
